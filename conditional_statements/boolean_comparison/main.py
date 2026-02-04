c097e743c2510fc49bd1a84e14cc41c28269c8bb
Tinn00 <>
2026-02-04 17:13:45 +0000

    Boolean Comparison

--- conditional_statements/boolean_comparison/main.py
+++ conditional_statements/boolean_comparison/main.py
@@ -2,4 +2,4 @@
 total_cost=25.00
 discountEligible = total_cost >= 20.00
 
-print (f"Is the purchase eligible for a discount?", {discountEligible}) 
+print (f"Is the purchase eligible for a discount? {discountEligible}") 
